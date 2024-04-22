d253ee727e3e3a7d08c5b034fd0aa135ebaf2fd1
Anurag Tandon <135813224+AnuragTandon1@users.noreply.github.com>
2024-04-22 15:17:20 +0530

    Update db.js
    Signed-off-by: Anurag Tandon <135813224+AnuragTandon1@users.noreply.github.com>

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connection  = mongoose.createConnection('mongodb+srv://anuragtandon54321:anu1122gamer@cluster0.htkbsue.mongodb.net/myshishu').on('open',()=>{
+const connection  = mongoose.createConnection().on('open',()=>{
     console.log("MongoDb connected");
 }).on('error',()=>{
     console.log("Mongo error");
